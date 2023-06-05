eedaf9749f36d2424757845f3ed733af075bd043
isishome <isishome@hanmail.net>
2023-06-06 02:11:59 +0900

    update 230505

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -9,13 +9,13 @@ const d4 = import.meta.env.VITE_APP_D4_ORIGIN
     <div class="col col-sm-4">
       <q-btn flat :ripple="false" padding="0" type="a" :href="d4" class="no-hover fit">
         <q-img :src="d4800" :srcset="`${d4400} 400w,
-        ${d4800} 800w`" sizes="(max-width: 400px) 400w,
-              (min-width: 400px) and (max-width: 800px) 800w" :ratio="1" />
+                            ${d4800} 800w`" sizes="(max-width: 400px) 400w,
+                  (min-width: 400px) and (max-width: 800px) 800w" :ratio="1" />
         <div>
           <div class="q-pt-lg text-weight-bold text-h6 relative-position">
             디아블로 4
           </div>
-          <q-chip size="12px" text-color="white" label="준비중" color="red" />
+          <q-chip size="12px" text-color="white" label="BETA" color="blue" />
         </div>
       </q-btn>
     </div>
